ae9b013ad30451cb411926da62b285fba913d686
Sofia Tejada <sofianais@icloud.com>
2021-05-03 18:51:38 -0700

    add some CSS and JS

--- app.js
+++ app.js
@@ -45,12 +45,16 @@ button.addEventListener('click', () => {
     
     if (score === 3) {
         resultsGood.textContent = `Excellent, ${userName}! You got ${score} questions right. That's ${Math.floor(percentage)}%`;
+        resultsGood.style.display = 'block';
     } else if (score === 2){
         resultsGood.textContent = `Good job, ${userName}! You got ${score} questions right. That's ${Math.floor(percentage)}%`;
+        resultsGood.style.display = 'block';
     } else if (score === 1) {
         resultsBad.textContent = `You should read the article again, ${userName}! You got ${score} question right. That's ${Math.floor(percentage)}%`;
+        resultsBad.style.display = 'block';
     } else if (score === 0) {
         resultsBad.textContent = `Uh-oh, ${userName}! You got ${score} questions right. That's ${Math.floor(percentage)}%! Try again!`;
+        resultsBad.style.display = 'block';
     }
 
 });
