cf3e2717b9c2bec026520fcacf59659a509f8963
Sofia Tejada <sofianais@icloud.com>
2021-05-03 19:55:06 -0700

    run tests for fix function

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,5 @@
 // IMPORT MODULES under test here:
-import { countsAsAYes } from '../utils.js';
+import { countsAsAYes, fix } from '../utils.js';
 
 const test = QUnit.test;
 
@@ -8,14 +8,21 @@ test('checking to see if it returns true or false properly', (expect) => {
     // Set up your arguments and expectations
     const expected = true;
     const expectedFalse = false;
+    const fixTrue = true;
+    const fixFalse = false;
     
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = countsAsAYes('yes');
     const actualFalse = countsAsAYes('hello');
+    const actualFixTrue = fix('n');
+    const actualFixFalse = fix('y');
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
     expect.equal(actualFalse, expectedFalse);
+    expect.equal(actualFixTrue, fixTrue);
+    expect.equal(actualFixFalse, fixFalse);
+
 });
